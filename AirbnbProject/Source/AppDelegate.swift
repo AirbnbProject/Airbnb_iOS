36f9f5eba8b66009b391a7779e280be869af2433
김승진 <seungjin429@naver.com>
2018-08-09 15:52:07 +0900

    gitignore DS_Store 추가

--- AirbnbProject/Source/AppDelegate.swift
+++ AirbnbProject/Source/AppDelegate.swift
@@ -27,14 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let shouldOpen :Bool = FBSDKApplicationDelegate.sharedInstance().application(app,  open: url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?,annotation: options[UIApplicationOpenURLOptionsKey.annotation])
         return shouldOpen
     }
-    
-    
-
-//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-//
-//        FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationop], annotation: options[UIApplicationOpenURLOptionsKey])
-//
-//    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
